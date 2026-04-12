f136f76dbe1b634bb2b077061901059ce66c6dad
YuvrajArora06 <arorayuvraj14@gmail.com>
2026-04-12 20:41:04 +0530

    DAY - 49

---
+++ Q49.c
@@ -0,0 +1,84 @@
+/*Problem: BST Insert
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct TreeNode* buildTree(int* arr, int n) {
+    if (n == 0) return NULL;
+
+    struct TreeNode** nodes = (struct TreeNode**)malloc(n * sizeof(struct TreeNode*));
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1) {
+            nodes[i] = createNode(arr[i]);
+        } else {
+            nodes[i] = NULL;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+            if (leftIndex < n) {
+                nodes[i]->left = nodes[leftIndex];
+            }
+            if (rightIndex < n) {
+                nodes[i]->right = nodes[rightIndex];
+            }
+        }
+    }
+
+    struct TreeNode* root = nodes[0];
+    free(nodes);
+    return root;
+}
+
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    if (root == NULL) {
+        return createNode(val);
+    }
+    if (val < root->val) {
+        root->left = insertIntoBST(root->left, val);
+    } else {
+        root->right = insertIntoBST(root->right, val);
+    }
+    return root;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    struct TreeNode* root = buildTree(arr, n);
+    free(arr);
+
+    int val;
+    scanf("%d", &val);
+    root = insertIntoBST(root, val);
+
+    // You can add code here to print the tree or verify the insertion
+    return 0;
+}
+
