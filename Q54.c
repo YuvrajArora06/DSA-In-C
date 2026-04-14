c35280137a46b77abfe9b2f67a06e1bcf3ca52d7
YuvrajArora06 <arorayuvraj14@gmail.com>
2026-04-15 00:51:13 +0530

    DAY - 54

---
+++ Q54.c
@@ -0,0 +1,77 @@
+/*Problem Statement:
+Perform zigzag (spiral) level order traversal of a binary tree. Alternate levels should be traversed left-to-right and right-to-left.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 indicates NULL)
+
+Output Format:
+- Print traversal in zigzag order*/
+
+#include <stdio.h>
+#include <stdlib.h>
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct TreeNode* buildTree(int* arr, int n) {
+    if (n == 0) return NULL;
+
+    struct TreeNode** nodes = (struct TreeNode**)malloc(n * sizeof(struct TreeNode*));
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1) {
+            nodes[i] = createNode(arr[i]);
+        } else {
+            nodes[i] = NULL;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+            if (leftIndex < n) {
+                nodes[i]->left = nodes[leftIndex];
+            }
+            if (rightIndex < n) {
+                nodes[i]->right = nodes[rightIndex];
+            }
+        }
+    }
+
+    struct TreeNode* root = nodes[0];
+    free(nodes);
+    return root;
+}
+
+void zigzagLevelOrder(struct TreeNode* root) {
+    if (root == NULL) return;
+
+    // Implement zigzag level order traversal logic here
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct TreeNode* root = buildTree(arr, n);
+    zigzagLevelOrder(root);
+
+    free(arr);
+    return 0;
+}
+
